20b625c4c9c78444d05fea29555d038f39472214
Mikheil Lomidze <lomidzemikheili@gmail.com>
2025-05-19 09:58:49 +0400

    Add run subcommand.

---
+++ cmd/run.go
@@ -0,0 +1,40 @@
+/*
+Package cmd
+Copyright © 2025 Mikheil Lomidze
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run gnat load generator",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, world! 🐜 gnat is ready.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
